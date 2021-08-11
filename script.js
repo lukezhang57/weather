8f071c3bd813bb75a56c7f5479cf1dd44d9b14ca
Luke Zhang <lukezhangxc@gmail.com>
2021-08-11 18:30:22 -0500

    app is now working

--- script.js
+++ script.js
@@ -17,5 +17,27 @@ function getWeather(city){
 
 function displayWeather(details){
     document.querySelector('.weather').style.display = 'block';
-    document.querySelector('.temp').innerHTML =  `${Math.round(details.main.temp)}&deg;F`;
+    document.getElementById('city').innerHTML =  `${details.name}, ${details.sys.country}`;
+    document.getElementById('temp').innerHTML =  `${Math.round(details.main.temp)}&deg;F`;
+    document.getElementById('condition').innerHTML =  `${details.weather[0].main}`;
+    let condition = details.weather[0].main;
+    let icon = document.getElementById('icon')
+    let prevIcon = icon.className;
+    let newIcon;
+    if (condition === 'Clear'){
+        newIcon = 'far fa-sun';
+    }
+    else if (condition == 'Rain'){
+        newIcon = 'fas fa-cloud-rain';
+    }
+    else if (condition == 'Clouds'){
+        newIcon = 'fas fa-cloud-sun';
+    }
+    else if (condition == 'Thunderstorm'){
+        newIcon = 'fas fa-bolt';
+    }
+    else {
+        prevIcon = newIcon;
+    }
+    icon.className = icon.className.replace(prevIcon,newIcon);
 }
